de46235f755e712d00a3676f12ab706f522554f2
hyucksukim <aswe3444@gmail.com>
2022-06-09 01:53:06 +0900

    commit on "main"

--- Dockerfiles/Backend/app/app.py
+++ Dockerfiles/Backend/app/app.py
@@ -159,7 +159,7 @@ def get_item(asin):
 @app.get("/title/{asin}")
 def get_title(asin):
     items = get_item(asin)
-    return items[0]['title4444444']
+    return items[0]['title44444445']
     
     
 if __name__ == "__main__":
