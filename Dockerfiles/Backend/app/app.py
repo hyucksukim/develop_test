05df7848d52f2ae57dbcf8aa13d1f5bcb71d570f
hyucksukim <86274940+hyucksukim@users.noreply.github.com>
2022-06-11 22:15:39 +0900

    Update app.py

--- Dockerfiles/Backend/app/app.py
+++ Dockerfiles/Backend/app/app.py
@@ -153,7 +153,7 @@ def get_item(asin):
     collection = db["books"]
     query = {'asin': asin}
     cursor = collection.find(query, projection={'_id': False})
-    result = 11#loads(dumps(cursor))
+    result = 12#loads(dumps(cursor))
     return result
 
 @app.get("/title/{asin}")
